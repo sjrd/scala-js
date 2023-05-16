cc81327fae1488c7e87647a50841f2fdc25100d0
Sébastien Doeraene <sjrdoeraene@gmail.com>
2023-05-16 14:44:35 +0200

    Fix #4855: Avoid case-insensitive clashes for internal module IDs.
    When an internal module contains a single class, we name it after
    the class name. This is supposed to give unambiguous module IDs.
    However, it is not good enough on case-insensitive file systems,
    as we can have two classes whose names differ only in case.
    
    To avoid the issue, we prefix uppercase ASCII characters with
    a '-' and all non-ASCII characters with a '-' and their code
    point value.
    
    We have a similar issue for digest-based module IDs. Their
    'internal-' prefix must also be protected for collision in a
    case-insensitive way. For that, we can directly compare with
    prefixes of the IDs `equalsIgnoreCase`.

--- linker/shared/src/test/scala/org/scalajs/linker/frontend/modulesplitter/InternalModuleIDGeneratorTest.scala
+++ linker/shared/src/test/scala/org/scalajs/linker/frontend/modulesplitter/InternalModuleIDGeneratorTest.scala
@@ -22,32 +22,34 @@ import org.scalajs.linker.standard.ModuleSet.ModuleID
 /** Whitebox tests for `InternalModuleIDGenerator`. */
 class InternalModuleIDGeneratorTest {
   @Test def testForClassName(): Unit = {
-    val testPublicModuleIDs = List(ModuleID("test.Public"), ModuleID("test.OtherPublic"))
+    val testPublicModuleIDs = List(ModuleID("test.-Public"), ModuleID("test.-Other-Public"))
     val generator = new InternalModuleIDGenerator(testPublicModuleIDs)
 
     def test(expected: String, classNameString: String): Unit =
       assertEquals(expected, generator.forClassName(ClassName(classNameString)).id)
 
-    test("java.lang.String", "java.lang.String")
-    test("java.lang.StringBuilder", "java.lang.StringBuilder")
+    test("java.lang.-String", "java.lang.String")
+    test("java.lang.-String-Builder", "java.lang.StringBuilder")
 
-    test("test-S.foo--Bar", "test-S.foo--Bar")
+    test("test---S.foo-----Bar", "test-S.foo--Bar")
 
-    test("test.été", "test.été")
-    test("test.Été", "test.Été")
+    test("test.-u0000e9ét-u0000e9é", "test.été")
+    test("test.-u0000c9Ét-u0000e9é", "test.Été")
 
-    test("test.ǳ", "test.ǳ") // U+01F3 Latin Small Letter Dz
-    test("test.Ǳ", "test.Ǳ") // U+01F1 Latin Capital Letter Dz
-    test("test.ǲ", "test.ǲ") // U+01F2 Latin Capital Letter D with Small Letter Z
+    test("test.-u0001f3ǳ", "test.ǳ") // U+01F3 Latin Small Letter Dz
+    test("test.-u0001f1Ǳ", "test.Ǳ") // U+01F1 Latin Capital Letter Dz
+    test("test.-u0001f2ǲ", "test.ǲ") // U+01F2 Latin Capital Letter D with Small Letter Z
 
-    test("test.Public.", "test.Public")
-    test("test.OtherPublic.", "test.OtherPublic")
+    test("test.-Public.", "test.Public")
+    test("test.-Other-Public.", "test.OtherPublic")
   }
 
   @Test def testForDigest(): Unit = {
     val goodModuleID = ModuleID("good")
     val otherGoodModuleID = ModuleID("othergood")
     val collidingModuleID = ModuleID("internal-mod")
+    val collidingCaseInsensitiveModuleID = ModuleID("InTernal--mod")
+    val collidingCaseInsensitiveModuleID2 = ModuleID("İnTernal-mod") // U+0130 Latin Capital Letter I with Dot Above
 
     val digest = Array(0x12.toByte, 0x34.toByte, 0xef.toByte)
 
@@ -62,5 +64,17 @@ class InternalModuleIDGeneratorTest {
     val generator3 = new InternalModuleIDGenerator(List(collidingModuleID))
     val digestGenerator3 = generator3.makeFromDigestGenerator(List(goodModuleID))
     assertEquals("internal--1234ef", digestGenerator3.forDigest(digest).id)
+
+    val generator4 = new InternalModuleIDGenerator(List(collidingCaseInsensitiveModuleID))
+    val digestGenerator4 = generator4.makeFromDigestGenerator(List(goodModuleID))
+    assertEquals("internal---1234ef", digestGenerator4.forDigest(digest).id)
+
+    val generator5 = new InternalModuleIDGenerator(List(collidingCaseInsensitiveModuleID2))
+    val digestGenerator5 = generator5.makeFromDigestGenerator(List(goodModuleID))
+    assertEquals("internal--1234ef", digestGenerator5.forDigest(digest).id)
+
+    val generator6 = new InternalModuleIDGenerator(Nil)
+    val digestGenerator6 = generator6.makeFromDigestGenerator(Nil)
+    assertEquals("internal-1234ef", digestGenerator1.forDigest(digest).id)
   }
 }
