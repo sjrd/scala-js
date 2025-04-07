scala-wasm/scala-wasm

Add WasmFeatures configuration

ir/shared/src/main/scala/org/scalajs/ir/Trees.scala
@@ -1076,6 +1076,7 @@ object Trees {
     final val UseECMAScript2015Semantics = "core/useECMAScript2015Semantics"
     final val IsWebAssembly = "core/isWebAssembly"
     final val LinkerVersion = "core/linkerVersion"
+    final val WasmUseJavaScript = "core/wasmUseJavaScript"
   }
 
   // Atomic expressions

sjrd/scala-js

ir/shared/src/main/scala/org/scalajs/ir/Trees.scala
@@ -1197,6 +1197,7 @@ object Trees {
     final val UseECMAScript2015Semantics = "core/useECMAScript2015Semantics"
     final val IsWebAssembly = "core/isWebAssembly"
     final val LinkerVersion = "core/linkerVersion"
+    final val TargetPureWasm = "core/targetPureWasm"
   }
 
   // Atomic expressions