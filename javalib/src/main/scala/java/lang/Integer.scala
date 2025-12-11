4b86710b6cb4ffb9caa588f74fe16fe98c61c3ce
Sébastien Doeraene <sjrdoeraene@gmail.com>
2025-12-11 14:51:08 +0100

    Opt: Better profit from the int range checks optimization.

--- javalib/src/main/scala/java/lang/Integer.scala
+++ javalib/src/main/scala/java/lang/Integer.scala
@@ -318,7 +318,7 @@ object Integer {
 
   @inline // because radix is almost certainly constant at call site
   def toString(i: Int, radix: Int): String = {
-    if (radix == 10 || radix < Character.MIN_RADIX || radix > Character.MAX_RADIX) {
+    if (radix == 10 || (radix < Character.MIN_RADIX || radix > Character.MAX_RADIX)) {
       Integer.toString(i)
     } else {
       import js.JSNumberOps.enableJSNumberOps
