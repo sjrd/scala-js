f45c0e63f76d05605104d6d60b602610a274d53c
Rikito Taniguchi <rikiriki1238@gmail.com>
2026-01-01 12:47:38 +0900

    Change test-component-model to trait/implementation pattern

--- examples/helloworld-wasi/src/main/scala/helloworld/HelloWorld.scala
+++ examples/helloworld-wasi/src/main/scala/helloworld/HelloWorld.scala
@@ -1,11 +1,12 @@
 package helloworld
 
-import scalajs.component.annotation._
-import scalajs.{component => cm}
+import scala.scalajs.component.annotation._
+import scala.scalajs.{component => cm}
+import helloworld.exports.wasi.cli.v0_2_0.Run
 
-object HelloWorld {
-  @ComponentExport("wasi:cli/run@0.2.0", "run")
-  def run(): cm.Result[Unit, Unit] = {
+@ComponentImplementation
+object HelloWorld extends Run {
+  override def run(): cm.Result[Unit, Unit] = {
     println("Hello world!")
     new cm.Ok(())
   }
--- examples/test-component-model/src/main/scala/componentmodel/WorldImpl.scala
+++ examples/test-component-model/src/main/scala/componentmodel/WorldImpl.scala
@@ -1,6 +1,7 @@
 package componentmodel
 
 import scala.scalajs.{component => cm}
+import scala.scalajs.component.annotation._
 import scala.scalajs.component.unsigned._
 
 import componentmodel.imports.Basics._
@@ -12,8 +13,55 @@ import scala.scalajs.ComponentUtils.toEither
 
 import java.util.Optional
 
-object WorldImpl {
-  def run(): Unit = {
+@ComponentImplementation
+object WorldImpl extends World {
+
+  override def roundtripBasics1(a: cm.Tuple9[UByte, Byte, UShort, Short, UInt, Int, Float, Double, Char]):
+      cm.Tuple9[UByte, Byte, UShort, Short, UInt, Int, Float, Double, Char] = a
+
+  override def roundtripListU16(a: Array[UShort]): Array[UShort] = a
+
+  override def roundtripListPoint(a: Array[Point]): Array[Point] = a
+
+  override def roundtripListVariant(a: Array[C1]): Array[C1] = a
+
+  override def roundtripString(a: String): String = a
+
+  override def roundtripPoint(a: Point): Point = a
+
+  override def roundtripC1(a: C1): C1 = a
+
+  override def roundtripZ1(a: Z1): Z1 = a
+
+  override def testC1(a: C1): Unit = {}
+
+  override def roundtripEnum(a: E1): E1 = a
+
+  override def roundtripTuple(a: cm.Tuple2[C1, Z1]): cm.Tuple2[C1, Z1] = a
+
+  override def roundtripOption(a: Optional[String]): Optional[String] = a
+
+  override def roundtripDoubleOption(a: Optional[Optional[String]]): Optional[Optional[String]] = a
+
+  override def roundtripResult(a: cm.Result[Unit, Unit]): cm.Result[Unit, Unit] = a
+
+  override def roundtripStringError(a: cm.Result[Float, String]): cm.Result[Float, String] = a
+
+  override def roundtripEnumError(a: cm.Result[C1, E1]): cm.Result[C1, E1] = a
+
+  override def roundtripF1(a: F1): F1 = a
+
+  override def roundtripF2(a: F2): F2 = a
+
+  override def roundtripF3(a: F3): F3 = a
+
+  override def roundtripFlags(a: cm.Tuple2[F1, F1]): cm.Tuple2[F1, F1] = a
+
+  override def roundtripTuple2(a: cm.Tuple2[Int, String]): cm.Tuple2[Int, String] = a
+
+  override def roundtripTuple3(a: cm.Tuple3[Int, String, Boolean]): cm.Tuple3[Int, String, Boolean] = a
+
+  override def run(): Unit = {
 
     val start = System.currentTimeMillis()
     assert(1 == roundtripU8(1))
