afe7880474736b5f8bb04d7bc4ab5475e4509850
Rikito Taniguchi <rikiriki1238@gmail.com>
2026-01-13 01:16:35 +0900

    Make Result and Tuple scalalib independent

--- library/src/main/scala/scala/scalajs/wit/Result.scala
+++ library/src/main/scala/scala/scalajs/wit/Result.scala
@@ -14,6 +14,44 @@ package scala.scalajs.wit
 
 sealed trait Result[+A, +B]
 
-final case class Ok[A](value: A) extends Result[A, Nothing]
+final class Ok[A](val value: A) extends Result[A, Nothing] {
+  override def equals(other: Any): Boolean = {
+    other match {
+      case that: Ok[_] =>
+        if (this.value == null) that.value == null
+        else this.value.equals(that.value)
+      case _ => false
+    }
+  }
 
-final case class Err[B](value: B) extends Result[Nothing, B]
+  override def hashCode(): Int = {
+    if (value == null) 0 else value.hashCode()
+  }
+
+  override def toString(): String = "Ok(" + value + ")"
+}
+
+object Ok {
+  def apply[A](value: A): Ok[A] = new Ok(value)
+}
+
+final class Err[B](val value: B) extends Result[Nothing, B] {
+  override def equals(other: Any): Boolean = {
+    other match {
+      case that: Err[_] =>
+        if (this.value == null) that.value == null
+        else this.value.equals(that.value)
+      case _ => false
+    }
+  }
+
+  override def hashCode(): Int = {
+    if (value == null) 0 else value.hashCode()
+  }
+
+  override def toString(): String = "Err(" + value + ")"
+}
+
+object Err {
+  def apply[B](value: B): Err[B] = new Err(value)
+}
