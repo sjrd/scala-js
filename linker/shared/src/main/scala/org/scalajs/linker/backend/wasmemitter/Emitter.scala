3a6c2c8a3225f49531b2b1893fff938e2a8fe41e
Sébastien Doeraene <sjrdoeraene@gmail.com>
2026-08-31 15:10:34 +0200

    Fix minimal Wasm interop tests with optimized semantics.
    Test it, and also test Compliant semantics.

--- linker/shared/src/main/scala/org/scalajs/linker/backend/wasmemitter/Emitter.scala
+++ linker/shared/src/main/scala/org/scalajs/linker/backend/wasmemitter/Emitter.scala
@@ -643,6 +643,8 @@ object Emitter {
             StringArgConstructorName)
       },
 
+      callMethod(ObjectClass, toStringMethodName),
+
       // TODO Ideally we should not require these, but rather adapt to their absence
       instantiateClass(ClassClass, NoArgConstructorName),
       instantiateClass(JSExceptionClass, AnyArgConstructorName),
